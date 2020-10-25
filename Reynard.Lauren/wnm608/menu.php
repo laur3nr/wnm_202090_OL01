afb11f443fab9d37e7bd3501381d5f61599d0383
Lauren Reynard <lauren@Laurens-iMac.local>
2020-10-25 19:25:12 -0400

    module 7

---
+++ Reynard.Lauren/wnm608/menu.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>menu</title>
+</head>
+<body>
+
+</body>
+</html>
---
+++ Reynard.Lauren/wnm608/product_list.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Product List</title>
+
+	<meta name="viewport" content="width=device-width">
+
+	<link rel="stylesheet" type="text/css" href="../styleguide/styles.css">
+	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.0/css/all.css">
+
+
+</head>
+<body>
+ 
+		<header class="navbar">
+			<div class="container display-flex">
+				<div class="flex-none">
+					<h1>About Us</h1>
+				</div>
+				<div class="flex-stretch"></div>
+				<nav class="nav-flex flex-none">
+					<ul>
+						<li><a href="#">link 1</a></li>
+						<li><a href="#">link 2</a></li>
+						<li><a href="#">link 3</a></li>
+					</ul>
+				</nav>
+			</div>
+		</header>
+
+
+</body>
+</html>
